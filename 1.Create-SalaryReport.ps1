75a5dde68d4ce512f9140d0625511a3553c89387
Panda-Network <146161184+PandaNetworkCN@users.noreply.github.com>
2023-09-27 15:05:50 +0800

    Add files via upload

---
+++ 1.Create-SalaryReport.ps1
@@ -0,0 +1,50 @@
+﻿# 1. Need install wkhtmltopdf.exe x 64 from https://wkhtmltopdf.org/downloads.html
+#    C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe
+# 2. Need Install module ImportExcel Install-Module -Name ImportExcel via admin
+
+# Import the module
+Import-Module -Name ImportExcel
+
+#Get current running folder. 
+$currentFolder = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+# Specify the path to the Excel file
+$excelFilePath = "$currentFolder\salrylist.xlsx"
+
+# Read the Excel file
+$excelData = Import-Excel -Path $excelFilePath
+
+#$properties = $excelData | Get-Member -MemberType NoteProperty |Select-Object -ExpandProperty Name 
+$properties = $excelData | Get-Member -MemberType NoteProperty | Sort-Object { [int]($_.Name.Split('.')[0]) } |Select-Object -ExpandProperty Name 
+write-host $properties
+
+# Process each rowcls
+
+foreach ($row in $excelData) {
+
+   $ID = $row.'2.员工 ID'
+
+   if ($ID -ne $null)
+   {
+
+   $TxtFilePath = "$currentFolder\$ID.txt"
+   $pdfFilePath = "$currentFolder\$ID.pdf"
+
+   Write-Output "********************工资清单*****************" | Out-File -FilePath $TxtFilePath -Force
+
+   foreach ($property in $properties) {
+
+   Write-Output $property : $row.$property "`r`n" | Out-File -FilePath $TxtFilePath -Append -NoNewline
+
+   }
+   }
+
+#Convert the text file to a PDF using wkhtmltopdf
+Start-Process -FilePath "C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe" -ArgumentList $TxtFilePath , $pdfFilePath -WindowStyle Hidden
+
+}
+
+#Delete all txt files
+Start-Sleep -Seconds 10
+Remove-Item -Path $currentFolder\*.txt -Force
+
---
+++ 2.Send-SalaryReport.ps1
@@ -0,0 +1,57 @@
+﻿# Need install
+# https://wkhtmltopdf.org/downloads.html
+# C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe
+# Install module ImportExcel Install-Module -Name ImportExcel via admin
+
+# Import the module
+Import-Module -Name ImportExcel
+
+#Get current running folder. 
+$currentFolder = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+# Specify the path to the Excel file
+$excelFilePath = "$currentFolder\salrylist.xlsx"
+
+# Read the Excel file
+$excelData = Import-Excel -Path $excelFilePath
+
+#Get all properties for object $excelData 
+#$properties = $excelData | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name
+
+# Process each row
+foreach ($row in $excelData) {
+
+   $ID = $row.'2.员工 ID'
+
+   if($ID -ne $null)
+   {
+    #Get Emplyee ID as the default file name
+    $mail = $row.'23.Email'
+    $Name = $row.'1.姓名'
+    
+#PDF file path
+$pdfFilePath = "$currentFolder\$ID.pdf"
+
+#Email settings
+$smtpServer = "service.snapon.com"
+$senderEmail = "BeijingHR@snapon.com"
+$recipientEmail = $mail
+$emailSubject = "工资单"
+$emailBody = "
+$Name 收
+
+请查收附件工资单
+ 
+"
+echo $recipientEmail
+echo $pdfFilePath
+
+# Send the email with the PDF attachment
+Send-MailMessage -SmtpServer $smtpServer -From $senderEmail -To $recipientEmail -Subject $emailSubject -Body $emailBody -Attachments $pdfFilePath -Encoding UTF8 -Verbose
+
+}
+}
+
+
+
+
